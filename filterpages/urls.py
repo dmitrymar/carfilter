aabddec0e4a203bbfc454f31bff80b7464169f9f
Dmitry Mardukhayev <dmitrymar@yahoo.com>
2016-03-09 22:16:59 +0000

    Added view and template for detailed blog post as well as CSS for the site.

--- filterpages/urls.py
+++ filterpages/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^post/(?P<pk>\d+)/$', views.detail, name='detail'),
+    url(r'^overview/(?P<year>[0-9]+)/(?P<make>[a-z]+[-.]?[a-z]+)/(?P<model>[0-9a-z]+[-.]?[0-9a-z]+)/(?P<body>[0-9a-z]+[-.]?[0-9a-z]+)/$', views.overview, name='overview'),
+    # Vehicle overview page. Go to base url + overview/2016/honda/accord/
+    url(r'^overview/(?P<year>[0-9]+)/(?P<make>[a-z]+[-.]?[a-z]+)/(?P<model>[0-9a-z]+[-.]?[0-9a-z]+)/$', views.overview, name='overview'),
 ]
--- filterpages/views.py
+++ filterpages/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 # Create your views here.
+# example url: http://localhost:8000/
 def index(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'filterpages/index.html', {'posts': posts})
+
+# example url: http://localhost:8000/post/1/
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'filterpages/detail.html', {'post': post})
+
+# example url: http://localhost:8000/overview/2016/honda/accord/coupe/
+def overview(request, year, make, model, body=None):
+    return render(request, 'filterpages/overview.html')
