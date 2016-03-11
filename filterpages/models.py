f0523651f85d8ccbe9bac3ee6e0d34844264661d
Dmitry Mardukhayev <dmitrymar@yahoo.com>
2016-03-11 18:47:23 +0000

    added get_thumb_url function

--- filterpages/models.py
+++ filterpages/models.py
@@ -17,3 +17,29 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Carmodel(models.Model):
+    year = models.PositiveSmallIntegerField(default=2016)
+    make = models.CharField(max_length=60)
+    model = models.CharField(max_length=60)
+    body_type = models.CharField(max_length=60, default='sedan')
+    styles = models.PositiveSmallIntegerField(default=1)
+    lowest_price = models.PositiveIntegerField()
+    image_url = models.CharField(max_length=100)
+
+    def get_page_path(self):
+        pagepath = "/".join(["/overview", str(self.year), self.make, self.model, self.body_type])
+        return pagepath
+
+    def get_thumb_url(self):
+        thumb_path = "".join(["http://media.ed.edmunds-media.com/", self.image_url, "276.jpg"])
+        return thumb_path
+
+    def __str__(self):
+        return '%s %s %s %s %s %s %s' % (self.year,
+                                    self.make,
+                                    self.model,
+                                    self.styles,
+                                    self.lowest_price,
+                                    self.get_page_path(),
+                                    self.get_thumb_url())
